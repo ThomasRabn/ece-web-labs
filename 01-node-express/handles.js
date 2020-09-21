0569c7a0b66a70fd7cd1e41a9b735a209f1d7d84
Thomas <thomas.rabian@gmail.com>
2020-09-21 21:25:43 +0200

    Adding comments

--- 01-node-express/handles.js
+++ 01-node-express/handles.js
@@ -1,4 +1,6 @@
 module.exports = function(app){
+
+    // Function for the /hello?name=something route
     app.get('/hello', function(req, res){
         if (req.query.name == "Thomas") {
             res.send("Hello Thomas! That's a nice name you got there!\n");
@@ -9,6 +11,7 @@ module.exports = function(app){
         }
     });
 
+    // Function for the other routes such as localhost:8080/test
     app.get('/:urlID', function(req, res){
         if (req.params.urlID == "Thomas") {
             res.send("Hey Thomas! That's a nice name you got there!\n");
@@ -17,12 +20,13 @@ module.exports = function(app){
         }
     });
 
-
+    // Function defining the return message of the root
     app.get('/', function(req, res){
         res.send('Welcome! This website will salute you with a special message depending on the route of your GET request (example: localhost:8080/Peter) or if you add some query parameters with the ID "name" when using the /hello route (example: localhost:8080/hello?name=Jean). \n Special messages can be shown using the name "Thomas".');
     });
 
+    // Function for everything else -> All the 404 errors
     app.use(function (req,res,next){
-        res.status(404).send('Error 404! Make sure to have exactly 1 parameter in you GET request!\n');
+        res.status(404).send('404 Error! Make sure to have exactly 1 parameter in you GET request!\n');
     });
 }
