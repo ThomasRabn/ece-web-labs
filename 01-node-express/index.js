8b46147021182495508c15f9c62c321e6398a00a
Thomas <thomas.rabian@gmail.com>
2020-09-21 17:39:55 +0200

    Root message added

--- 01-node-express/index.js
+++ 01-node-express/index.js
@@ -12,6 +12,10 @@ app.get('/:urlID', function(req, res){
     res.send('Hello ' + req.params.urlID +'!\n');
 });
 
+app.get('/', function(req, res){
+    res.send('Welcome! This website will salute you with a special message depending on the first argument of your GET request (example: localhost:8080/Peter)');
+});
+
 app.use(function (req,res,next){
 	res.status(404).send('Error 404! Make sure to have exactly 1 parameter in you GET request!\n');
 });
