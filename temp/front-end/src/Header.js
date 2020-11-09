0ad07cf293d0bcf927ebe3b86918681aacae5210
ThomasRabn <thomas.rabian@gmail.com>
2020-11-09 16:02:11 +0100

    Ajout de la version pour le 10 nov

---
+++ temp/front-end/src/Header.js
@@ -0,0 +1,18 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+
+const styles = {
+    header: {
+        height: '60px',
+        backgroundColor: 'rgba(255,255,255,.3)',
+        flexShrink: 0,
+    }
+}
+
+export default () => {
+    return (
+        <header className="App-header" css={styles.header}>
+            <h1>header</h1>
+        </header>
+    );
+}
