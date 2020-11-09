9984e8dce2f4b96018ff83e924a28ea79a175c0c
ThomasRabn <thomas.rabian@gmail.com>
2020-11-09 20:24:22 +0100

    Je vais manger

--- temp/front-end/src/Channels.js
+++ temp/front-end/src/Channels.js
@@ -1,5 +1,8 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
+import { useEffect, useState } from 'react';
+const axios = require('axios');
+
 
 const styles = {
     channels: {
@@ -7,9 +10,40 @@ const styles = {
     }
 }
 
+// function getChannels() {
+//     var value;
+//     axios.get('localhost:3001/channels')
+//         .then((response) => {
+//             value = response;
+//         });
+//     return value;
+// }
+
 export default () => {
+    const [channels, setChannels] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            let { data } = await axios.get('localhost:3001/channels', {
+                headers: {
+                    'Access-Control-Allow': '*',
+                    'Access-Control-Allow-Credentials': 'true'
+                }
+            });
+            console.log(data);
+            setChannels(data);
+        }
+        fetchData();
+    }, [])
     return (
         <div css={styles.channels}>
+            <ul>
+                {channels.map((channel, i) => (
+                    <li key={i}>
+                        <a href='localhost:3000/'>{channel.name}</a>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
