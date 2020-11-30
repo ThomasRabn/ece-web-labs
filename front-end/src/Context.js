ef6bbba2c28ddd7326bda68bb74ae3cf94b08d6c
ThomasRabn <thomas.rabian@gmail.com>
2020-11-30 23:52:29 +0100

    Changes on context, adding cookies, and login has changed. This is a non working login version for now

--- front-end/src/Context.js
+++ front-end/src/Context.js
@@ -1,12 +1,28 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
+import { useCookies } from 'react-cookie';
 
 export const Context = createContext();
 
 export default ({
     children
 }) => {
-    
+    const [oauth, setOauth] = useState(null);
+    const [cookies, setCookie, removeCookie] = useCookies([]);
     return (
-        <Context.Provider value={{test: 'hello'}}>{children}</Context.Provider>
+        <Context.Provider value={{
+            oauth: oauth,
+            setOauth: () => {
+                if(oauth) {
+                    const { id_token } = oauth;
+                    const id_payload = id_token.split('.')[1];
+                    const { email } = JSON.parse(atob(id_payload));
+                    setCookie('oauth', oauth);
+                } else {
+                    removeCookie('oauth');
+                }
+                setOauth(oauth);
+            }
+        }}
+        >{children}</Context.Provider>
     )
 }
--- front-end/src/Login.js
+++ front-end/src/Login.js
@@ -1,58 +1,16 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
+import { useCookies} from 'react-cookie';
+import { useEffects } from 'react';
 // Core
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
 // Icons 
-import Send from '@material-ui/icons/Send';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import LockIcon from '@material-ui/icons/Lock';
-import { Route, Router, Link } from 'react-router-dom';
+import qs from 'qs';
+import axios from 'axios';
+import crypto from 'crypto';
 // Layout
 import { useTheme } from '@material-ui/core/styles';
-
-const crypto = require('crypto');
-
-const base64URLEncode = (str) => {
-    return str.toString('base64')
-        .replace(/\+/g, '-')
-        .replace(/\//g, '_')
-        .replace(/=/g, '');
-}
-
-const sha256 = (buffer) => {
-    return crypto
-        .createHash('sha256')
-        .update(buffer)
-        .digest();
-}
-
-const handler = () => {
-    const authorization_endpoint = "http://127.0.0.1:5556/dex/auth";
-    const client_id = "example-app";
-    const redirect_uri = "http://127.0.0.1:5555/callback";
-    const scope = "openid%20email%20offline_access";
-    const code_verifier = base64URLEncode(crypto.randomBytes(32));
-    console.log(code_verifier);
-    const code_challenge = base64URLEncode(sha256(code_verifier));
-    const url = authorization_endpoint+"?client_id="+client_id+"&scope="+scope
-                +"&response_type=code&redirect_uri="+redirect_uri+"&code_challenge="+code_challenge
-                +"&code_challenge_method=S256";
-    const data = {
-        code_verifier: code_verifier,
-        url: url
-    }
-    console.log(JSON.stringify(data, null, 2)); 
-    console.log('\n\n');
-    return data;
-}
-
-const onRegister = () => {
-    const loginValue = handler();
-    console.log(loginValue);
-    this.props.history.push(loginValue.url);
-};
 
 const useStyles = (theme) => ({
     root: {
@@ -89,13 +47,34 @@ const useStyles = (theme) => ({
     }
 });
 
-export default ({
-    onUser
+
+const base64URLEncode = (str) => {
+    return str.toString('base64')
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=/g, '');
+}
+
+const sha256 = (buffer) => {
+    return crypto
+        .createHash('sha256')
+        .update(buffer)
+        .digest();
+}
+
+const Redirect = ({
+    config,
+    codeVerifier
 }) => {
     const styles = useStyles(useTheme());
-    const linkToLogin = handler();
-    console.log("Link to login :");
-    console.log(linkToLogin);
+    const redirect = (e) => {
+        e.stopPropagation();
+        const code_challenge = base64URLEncode(sha256(codeVerifier));
+        const url = config.authorization_endpoint+"?client_id="+config.client_id+"&scope="+config.scope
+                +"&response_type=code&redirect_uri="+config.redirect_uri+"&code_challenge="+code_challenge
+                +"&code_challenge_method=S256";
+        window.location = url;
+    }
     return (
         <div css={styles.root}>
             <div css={styles.centered}>
@@ -108,18 +87,11 @@ export default ({
                             variant="contained"
                             color="secondary"
                             endIcon={<AccountCircle/>}
-                            href={linkToLogin.url}
-                            // onClick={(e) => {
-                            //     e.stopPropagation();
-                            //     onRegister();
-                            //     //     //onUser({ username: 'david'})
-                            // }}
+                            onClick= { redirect() }
                         >
                             Login
                     </Button> 
-                </div>
-
-                {/* <fieldset>
+                    {/* <fieldset>
                     <Grid container spacing={1} alignItems="flex-end">
                         <Grid item>
                             <AccountCircle />
@@ -162,6 +134,87 @@ export default ({
                         Send
                     </Button>
                 </fieldset> */}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+// const handler = () => {
+    
+//     const code_verifier = base64URLEncode(crypto.randomBytes(32));
+//     console.log(code_verifier);
+//     const code_challenge = base64URLEncode(sha256(code_verifier));
+    
+//     const data = {
+//         code_verifier: code_verifier,
+//         url: url
+//     }
+//     console.log(JSON.stringify(data, null, 2)); 
+//     console.log('\n\n');
+//     return data;
+// }
+
+// const Token = ({
+//     oauth
+// }) => {
+//     const [,, removeCookie] = useCookies([]);
+//     const styles = useStyles(useTheme());
+//     const logout = (e) => { 
+//         e.stopPropagation();
+//         removeCookie('oauth');
+//     }
+//     return (
+//         <div css={styles.root}>
+//             Welcome {email} {/*<Link onClick={logout} color='secondary'>Logout</Link> */}
+//         </div>
+//     )
+// }
+
+
+export default ({
+    onUser
+}) => {
+    const styles = useStyles(useTheme());
+
+    const config = {
+        authorization_endpoint: 'http://127.0.0.1:5556/dex/auth',
+        client_id: 'example-app',
+        redirect_uri: 'http://127.0.0.1:5555/callback',
+        scope: 'openid%20email%20offline_access',
+    }
+    const code_verifier = base64URLEncode(crypto.randomBytes(32));
+
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get('code');
+    
+    if(!code) { // We are not redirected from an Oauth server 
+        // if(!cookies.oauth) { // If the user is not connected
+        //     Redirect(config, code_verifier);
+        // } else {
+        //     // The user is connected, great!
+        // }
+    } else {
+        
+    }
+
+    return (
+        <div css={styles.root}>
+            <div css={styles.centered}>
+                <div style={{textAlign: 'center'}}>
+                    <h1 css={styles.welcomeTitle}>Welcome</h1>
+                    <h2 css={styles.loginTitle}>Please log in</h2>
+                    <Button
+                            // style={{marginLeft: "auto"}}
+                            type="input"
+                            variant="contained"
+                            color="secondary"
+                            endIcon={<AccountCircle/>}
+                        >
+                            Login
+                    </Button> 
+                </div>
             </div>
         </div>
     );
