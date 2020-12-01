756cd8188316a81fda9e22a7b115ee862e5d155a
ThomasRabn <thomas.rabian@gmail.com>
2020-12-01 13:08:08 +0100

    Little modifications

--- front-end/src/Context.js
+++ front-end/src/Context.js
@@ -8,21 +8,21 @@ export default ({
 }) => {
     const [oauth, setOauth] = useState(null);
     const [cookies, setCookie, removeCookie] = useCookies([]);
-    return (
-        <Context.Provider value={{
-            oauth: oauth,
-            setOauth: () => {
-                if(oauth) {
-                    const { id_token } = oauth;
-                    const id_payload = id_token.split('.')[1];
-                    const { email } = JSON.parse(atob(id_payload));
-                    setCookie('oauth', oauth);
-                } else {
-                    removeCookie('oauth');
-                }
-                setOauth(oauth);
-            }
-        }}
-        >{children}</Context.Provider>
-    )
+    // return (
+    //     <Context.Provider value={{
+    //         oauth: oauth,
+    //         setOauth: () => {
+    //             if(oauth) {
+    //                 const { id_token } = oauth;
+    //                 const id_payload = id_token.split('.')[1];
+    //                 const { email } = JSON.parse(atob(id_payload));
+    //                 setCookie('oauth', oauth);
+    //             } else {
+    //                 removeCookie('oauth');
+    //             }
+    //             setOauth(oauth);
+    //         }
+    //     }}
+    //     >{children}</Context.Provider>
+    // )
 }
