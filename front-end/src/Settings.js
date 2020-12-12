9fe8d9d25d5263cc74ab3cca6f1ddff8e356a60b
thomasbastide <thomas.bastide2@orange.fr>
2020-12-12 16:34:59 +0100

    Settings page

---
+++ front-end/src/Settings.js
@@ -0,0 +1,7 @@
+import {} from 'react';
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+// Layout
+import { useTheme } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
